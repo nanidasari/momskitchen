cbef3434d33d122edce8a573fd6b13e0426e072e
Isaac Dasari <90327970+nanidasari@users.noreply.github.com>
2022-09-30 12:45:05 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,28 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks","tablinksl");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+function opn(){
+        
+  document.getElementById("mnav").style.marginLeft = "0px";
+  
+  
+}
+function cls(){
+  
+  document.getElementById("mnav").style.marginLeft = "-500px";
+}
